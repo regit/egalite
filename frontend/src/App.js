d43aad475e23171300933e6f553f1e31a2605456
Eric Leblond <eric@regit.org>
2018-03-04 00:27:43 +0100

    frontend: basic listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      organizations: []
+    };
+  }
+
+  componentDidMount() {
+        axios.get('http://localhost:8080/' + 'api/organization/')
+      .then(res => {
+        this.setState({ organizations: res.data });
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">L'égalité c'est maintenant</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="organizations-list">
+          <ul>
+          {this.state.organizations.map(function(orga) {
+            return(
+            <Organization key={orga.pk} data={orga} />
+            )
+          })}
+          </ul>
+        </div>
       </div>
     );
+  }
+}
+
+class Organization extends Component {
+  render() {
+    return (
+      <div className="organization">
+        <h2>{this.props.data.name}</h2>
+        <ul>
+          <li>IEGH: {this.props.data.iegh}</li>
+        </ul>
+      </div>
+    )
   }
 }
 
