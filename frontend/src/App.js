9c745b4d7bdb599c2e179642e3f00594b296433b
Eric Leblond <eric@regit.org>
2018-03-04 16:39:57 +0100

    frontend: better clickable area on orga list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -80,8 +80,8 @@ class Organization extends Component {
   render() {
     var orga_iegh = Number(this.props.data.iegh).toFixed(1);
     return (
-      <div className="organization">
-        <h5 onClick={this.handleClick}>{this.props.data.name}</h5>
+      <div className="organization" onClick={this.handleClick} style={{cursor:'pointer'}}>
+        <h5>{this.props.data.name}</h5>
         <div className="progress">
           <div className="progress-bar" role="progressbar" style={{ width: orga_iegh + '%' }} aria-valuenow="{ orga_iegh}" aria-valuemin="0" aria-valuemax="100">{ orga_iegh }</div>
         </div>
