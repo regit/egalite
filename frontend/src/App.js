7de7481ba56d09bff2065a90ae0e01a4b1a88605
Eric Leblond <eric@regit.org>
2018-07-19 14:50:21 +0200

    update pre mise en prod 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import C3Chart from 'react-c3js';
 import logo from './logo.svg';
 import cartoon from './images/cartoon.jpg';
+import formula from './images/iegh-formula.png';
 import './App.css';
 import * as config from './config/Api.js';
 
@@ -81,7 +82,9 @@ class IEHGIndex extends Component {
         <div className="container-fluid">
           <div className="row">
 	     <div className="col-sm-12">
-	        <p className="text-justify">Les obligations des entreprises en termes d'égalité professionnelle sont désormais inscrites dans la loi et concernent notamment celles qui comptent 50 salariés et plus. Malgré ces nombreuses mesures, la résistance du plafond de verre perdure. En effet, si la « Loi Copé-Zimmermann » fixe un quota obligatoire de 40% du sexe sous-représenté dans les Conseils d'Administration au 1er janvier 2017 dans les entreprises cotées et dans les sociétés comptant plus de 500 salariés, la promotion interne des entreprises n'est pas véritablement encadrée. Or une vraie mixité dans les instances dirigeantes est un enjeu de compétitivité et de performance économique et sociale. Il est en outre important que les femmes et les hommes prennent pleinement conscience des progrès qui restent à accomplir.</p>
+	        <p className="text-justify">Les obligations des entreprises en termes d'égalité professionnelle sont désormais inscrites dans la loi et concernent notamment celles qui comptent 50 salariés et plus. Malgré ces nombreuses mesures, la résistance du plafond de verre perdure. En effet, si la « Loi Copé-Zimmermann » fixe un quota obligatoire de 40% du sexe sous-représenté dans les Conseils d'Administration au 1er janvier 2017 dans les entreprises cotées et dans les sociétés comptant plus de 500 salariés, la promotion interne des entreprises n'est pas véritablement encadrée. Or une vraie mixité dans les instances dirigeantes est un enjeu de compétitivité et de performance économique et sociale. Il est en outre important que les femmes et les hommes prennent pleinement conscience des progrès qui restent à accomplir.
+		</p>
+<p className="text-justify">La présente plateforme vise à établir un état des lieux des chances de promotion des femmes dans les grandes entreprises, par la voie d'un indice, l'IEHG. Chacune et chacun peut contribuer à ce suivi en renseignant les données - si possible publiques - relatives aux grands groupes.</p>	
 	     </div>
 	  </div>
           <div className="row">
@@ -95,7 +98,10 @@ class IEHGIndex extends Component {
                    des chances de promotion au sein d'une organisation. Il mesure l'écart entre
                    la répartition globale des sexes et la composition des instances dirigeantes (comité exécutif, secrétariat général).
                    Si l'IEHG vaut 100 alors les chances de promotions d'un homme ou d'une femme sont égales. Si il vaut 0 alors un genre
-                   maintient l'autre sous une domination totale.
+                   maintient l'autre sous une domination totale.</p>
+                 <p className="card-text text-justify">
+		   La formule de calcul de l'IEGH est la suivante:
+                    <img src={formula} className="App-cartoon" alt="cartoon" title="Formule de l'IEGH." />
                 </p>
                </div>
              </div>  
