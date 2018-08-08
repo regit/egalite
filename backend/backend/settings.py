6c5ea672fd71dfa4a1f632c9c45c0ca4f6f67446
Eric Leblond <eric@regit.org>
2018-08-08 12:23:04 +0200

    backend: add pagination

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,3 +128,8 @@ WEBPACK_LOADER = {
             'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.dev.json'),
         }
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}
